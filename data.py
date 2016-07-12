43d7e90ecddefd2c51baf2976acdc8d819eb400a
Jeremy <jloxfo2@gmail.com>
2016-07-12 13:27:05 -0500

    added a ADVMData class, ADVMProcParam class, ADVMConfigParam class to data.py

--- data.py
+++ data.py
@@ -1,6 +1,305 @@
 import re
 
 import pandas as pd
+import numpy as np
+import copy
+
+
+class ADVMProcParam:
+    """
+    Stores ADVM Processing parameters.
+
+    """
+
+    def __init__(self, num_cells):
+        """
+
+        :param num_cells: Number of cells reported by the ADVM configuration parameters
+
+        Note: The number of cells is required because it is used for a boundary check
+            when setting the 'Minimum Number of Cells' value.
+        """
+
+        self._number_of_cells = num_cells
+        self._proc_dict = {
+            "Beam": 2,
+            "Moving Average Span": 1,
+            "Backscatter Values": "SNR",
+            "Minimum Cell Mid-Point Distance": -np.inf,
+            "Maximum Cell Mid-Point Distance": np.inf,
+            "Minimum Number of Cells": 1,
+            "Minimum Vbeam": -np.inf,
+            "Near Field Correction": True,
+            "WCB Profile Adjustment": True
+        }
+
+    def __getitem__(self, key):
+        """
+        Return the requested processing parameter value.
+
+        :param key: Processing parameter key
+        :return: Processing parameter corresponding to the given key
+        """
+
+        self._check_proc_key(key)
+        return self._proc_dict[key]
+
+    def __setitem__(self, key, value):
+        """
+        Store the requested processing parameter value.
+
+        :param key: Processing parameter key
+        :param value: Processing value to be stored
+        :return: Nothing
+        """
+
+        self._check_proc_key(key)
+        self._check_proc_value(key, value)
+        self._proc_dict[key] = value
+        return
+
+    def __delitem__(self, key):
+        """
+        Delete the requested processing parameter.
+
+        :param key: Processing parameter key
+        :return: Nothing
+        """
+
+        self._proc_dict.pop(key, None)
+        return
+
+    def _check_proc_key(self, key):
+        """
+        Check if the provided key exists in the proc_dict. Raise an exception if not.
+
+        :param key: User-provided processing dictionary key
+        :return: Nothing
+        """
+
+        if key not in self._proc_dict.keys():
+            raise KeyError(key)
+        return
+
+    def _check_proc_value(self, key, value):
+        """
+        Check if the value provided is valid. Raise an exception if not.
+
+        :param key: User-provided processing dictionary key
+        :param value: User-provided processing dictionary value
+        :return: Nothing
+        """
+
+        if key == "Beam" and (value in range(1, 3) or value == 'Avg'):
+            return
+        elif key == "Moving Average Span" and (0 <= value <= 1):
+            return
+        elif key == "Backscatter Values" and (value == "SNR" or value == "Amp"):
+            return
+        elif key == "Minimum Cell Mid-Point Distance":
+            np.float(value)
+            return
+        elif key == "Maximum Cell Mid-Point Distance":
+            np.float(value)
+            return
+        elif key == "Minimum Number of Cells" and (1 <= value <= self._number_of_cells):
+            return
+        elif key == "Minimum Vbeam":
+            np.float(value)
+            return
+        elif key == "Near Field Correction" and isinstance(value, bool):
+            return
+        elif key == "WCB Profile Adjustment" and isinstance(value, bool):
+            return
+        else:
+            raise ValueError(value)
+
+
+class ADVMConfigParam:
+    """
+    Stores ADVM Configuration parameters.
+
+    """
+
+    def __init__(self, config_dict):
+        """
+
+        :param config_dict: Dictionary containing ADVM configuration data required to process ADVM data.
+        """
+
+        self._config_dict = copy.deepcopy(config_dict)
+
+    def __getitem__(self, key):
+        """
+        Return the requested configuration parameter value.
+
+        :param key: Configuration parameter key
+        :return: Configuration parameter corresponding to the given key
+        """
+
+        self._check_config_key(key)
+        return self._config_dict[key]
+
+    def __setitem__(self, key, value):
+        """
+        Store the requested configuration parameter value.
+
+        :param key: Configuration parameter key
+        :param value: Configuration value to be stored
+        :return: Nothing
+        """
+
+        self._check_config_key(key)
+        self._config_dict[key] = value
+        return
+
+    def __delitem__(self, key):
+        """
+        Delete the requested configuration parameter.
+
+        :param key: Configuration parameter key
+        :return: Nothing
+        """
+
+        self._config_dict.pop(key, None)
+        return
+
+    def _check_config_key(self, key):
+        """
+        Check if the provided key exists in the config_dict. Raise an exception if not.
+
+        :param key: Configuration dictionary key
+        :return: Nothing
+        """
+
+        if key not in self._config_dict.keys():
+            raise KeyError(key)
+        return
+
+
+class ADVMData:
+    """
+    Stores ADVM data and parameters.
+
+    """
+
+    def __init__(self, config_dict, acoustic_df):
+        """
+        Initializes ADVMData instance. Creates default processing configuration data attribute.
+
+        :param config_dict: Dictionary containing ADVM configuration data required to process ADVM data
+        :param acoustic_df: DataFrame containing acoustic data
+        """
+
+        self._acoustic_df = copy.deepcopy(acoustic_df)
+        self._config_dict = ADVMConfigParam(config_dict)
+        self._proc_dict = ADVMProcParam(config_dict["Number of Cells"])
+
+    def set_proc_params(self, proc_params):
+        """
+        Sets the processing parameters based on user input.
+
+        :param proc_params: Dictionary containing configuration parameters
+        :return: Nothing
+        """
+
+        for key in proc_params.keys():
+            self._proc_dict[key] = proc_params[key]
+
+        return
+
+    def get_config_params(self, config_params):
+        """
+        Find and return configuration parameters. Throw ValueError if config_params contains invalid key.
+
+        :param config_params: List containing configuration parameters (keys)
+        :return: Dictionary containing configuration parameters
+        """
+
+        requested_params = {}
+
+        # Set return values for all matching keys.
+        for key in config_params:
+            requested_params[key] = self._config_dict[key]
+
+        return requested_params
+
+    def get_proc_params(self, proc_params):
+        """
+        Find and return processing parameters. Throws ValueError if proc_params contains invalid key.
+
+        :param proc_params: List containing processing parameters (keys)
+        :return: Dictionary containing processing parameters
+        """
+
+        requested_params = {}
+
+        # Set return values for all matching keys.
+        for key in proc_params:
+            requested_params[key] = self._proc_dict[key]
+
+        return requested_params
+
+    def get_meanSCB(self):
+        """
+        Return mean sediment corrected backscatter. Throw exception if all required variables have not been provided.
+
+        :return: Mean sediment corrected backscatter for all observations contained in acoustic_df
+        """
+
+        return pd.Series(index=self._acoustic_df.index.values)
+
+
+    def get_cell_range(self):
+        """
+        Calculate range of cells along a single beam.
+
+        :return: Range of cells along a single beam
+        """
+
+        return pd.Series(index=self._acoustic_df.index.values)
+
+    def get_MB(self):
+        """Return measured backscatter"""
+
+        return
+
+    def get_SAC(self):
+        """
+        Calculate sediment attenuation coefficient. Throw exception if all required variables have not been provided.
+
+        :return: Sediment attenuation coefficient for all observations in acoustic_df
+        """
+
+        return pd.Series(index=self._acoustic_df.index.values)
+
+    def get_SCB(self):
+        """
+        Calculate sediment corrected backscatter. Throw exception if all required variables have not been provided.
+
+        :return: Sediment corrected backscatter for all cells in the acoustic time series
+        """
+
+        return pd.Series(index=self._acoustic_df.index.values)
+
+    def get_WCB(self):
+        """
+        Calculate water corrected backscatter. Throw exception if all required variables have not been provided
+
+        :return: Water corrected backscatter for all cells in the acoustic time series
+        """
+
+        return pd.Series(index=self._acoustic_df.index.values)
+
+    def merge(self, other):
+        """
+        Merges self and other ADVMData objects. Throws exception if other ADVMData object is incompatible with self.
+
+        :param other: ADVMData object to be merged with self
+        :return: Merged ADVMData object
+        """
+
+        return pd.Series(index=self._acoustic_df.index.values)
 
 
 class RawAcousticDataContainer:
@@ -19,7 +318,7 @@ class RawAcousticDataContainer:
         """ Add acoustic data to container."""
 
         # get the frequency of the ADVM data set
-        frequency = new_advm_data.get_config_params()['frequency']
+        frequency = new_advm_data.get_config_params()['Frequency']
 
         # if a data set with the frequency is already loaded,
         if frequency in self._acoustic_data.keys():
@@ -159,7 +458,7 @@ class RawAcousticDataConverter:
 
             # get a data frame containing the columns of the raw
             # acoustic data
-            raw_data_df = advm_df.loc[:,raw_data_cols]
+            raw_data_df = advm_df.loc[:, raw_data_cols]
 
             # drop the columns from the input data frame
             raw_data_df.drop(raw_data_cols, inplace=True)
