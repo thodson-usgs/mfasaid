f99cebc40f541d3114ba838cb5f8814fdb06d3be
Marian Domanski <mdomanski@usgs.gov>
2018-03-02 16:08:25 -0600

    Updated for new advmdata organization

--- said/acoustic.py
+++ said/acoustic.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from advmdata import ADVMParam
+from advmdata.core import ADVMParam
 from linearmodel.datamanager import DataManager
 
 import said.surrogatemodel as surrogatemodel
